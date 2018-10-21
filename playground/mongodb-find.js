6ba9512ffd4c6c7cc87692a735c258e8f76009a5
V S Bharath <bharathcse5t6@gmail.com>
2018-10-21 22:37:17 +0530

    fetching data

---
+++ playground/mongodb-find.js
@@ -0,0 +1,18 @@
+//const MongoClient = require('mongodb').MongoClient
+const {MongoClient,ObjectID} = require('mongodb')
+
+//client gives you access to db //db gives access to read or write data
+MongoClient.connect("mongodb://localhost:27017/todoApp",(err,db)=>{
+  if(err) {
+     return console.log("unable to connect to db")
+  }
+  console.log('connected to mongodb')
+  db.collection('Todos').find().toArray().then((docs)=>{
+    console.log(docs )
+  },(err)=>{
+      console.log(err)
+  })
+
+
+  //db.close()
+})
