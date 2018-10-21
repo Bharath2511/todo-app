992367e0a3c36dcfdc8048ae77fb618ed0b7fe42
V S Bharath <bharathcse5t6@gmail.com>
2018-10-21 22:09:57 +0530

    insert one

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,5 +6,15 @@ MongoClient.connect("mongodb://localhost:27017/todoApp",(err,db)=>{
   }
   console.log("connected to db")
 
+  db.collection('Todos').insertOne({
+   text : "something to do",
+   completed : false
+  },(err,result)=>{
+   if(err) {
+       return console.log('unable to insert',err)
+   }
+   console.log(JSON.stringify(result.ops,undefined,2))
+  })
+
   db.close()
 })
